f6c2338c79e47b7529c4167cb561fe0f22a0011b
Mu Abdullah <mohamed553231@gmail.com>
2026-01-27 23:19:55 +0200

    add FetchNewestBooksUseCase to retrieve the newest books from the repo

---
+++ lib/Features/home/presentation/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/Features/home/presentation/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/presentation/domain/repos/home_repo.dart';
+import 'package:bookly/Features/home/presentation/domain/use_cases/use_case.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParams> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParams? params]) {
+    return homeRepo.fetchNewestBooks();
+  }
+}
