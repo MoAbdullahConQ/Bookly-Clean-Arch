c57febbdb55fbbf40c5647c2da74c9205c694fc0
Mu Abdullah <mohamed553231@gmail.com>
2026-01-27 22:46:38 +0200

    book entity Done

---
+++ lib/Features/home/presentation/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity {
+  final String bookImage;
+  final String bookTitle;
+  final String authorName;
+  final num bookPrice;
+  final num bookRating;
+
+  BookEntity(this.bookImage, this.bookTitle, this.authorName, this.bookPrice,
+      this.bookRating);
+}
