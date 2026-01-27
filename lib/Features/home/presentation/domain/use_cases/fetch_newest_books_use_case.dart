2f7b9f2af3065335945c6c2a255a95977636b92e
Mu Abdullah <mohamed553231@gmail.com>
2026-01-27 23:33:24 +0200

    refactor: UseCase move UseCase to core and update imports in featured and newest books use cases

--- lib/Features/home/presentation/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/Features/home/presentation/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/Features/home/presentation/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/presentation/domain/repos/home_repo.dart';
-import 'package:bookly/Features/home/presentation/domain/use_cases/use_case.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
