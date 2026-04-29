e629174d61391b29d52b8f4684ca38643a112115
Mu Abdullah <mohamed553231@gmail.com>
2026-04-29 09:26:16 +0300

    Refactor CustomSearchTextField to use a debounce timer for search input handling

--- lib/Features/search/presentation/views/widgets/custom_search_text_field.dart
+++ lib/Features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -1,16 +1,40 @@
+import 'dart:async';
 import 'package:bookly/Features/search/presentation/manager/search_books_cubit/search_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CustomSearchTextField extends StatelessWidget {
+class CustomSearchTextField extends StatefulWidget {
   const CustomSearchTextField({super.key});
+
+  @override
+  State<CustomSearchTextField> createState() => _CustomSearchTextFieldState();
+}
+
+class _CustomSearchTextFieldState extends State<CustomSearchTextField> {
+  Timer? debounce;
+  //Every time the user types a character, we cancel the old timer and start a new one.
+  // If the user stops typing for 500ms — the request is sent.
+  void onSearchChanged(String query, BuildContext context) {
+    // Cancel the previous timer if the user is still typing
+    if (debounce?.isActive ?? false) debounce!.cancel();
+    debounce = Timer(const Duration(milliseconds: 500), () {
+
+      BlocProvider.of<SearchBooksCubit>(context).searchForBooks(query: query);
+    });
+  }
+
+  @override
+  void dispose() {
+    debounce?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       onChanged: (query) {
-        BlocProvider.of<SearchBooksCubit>(context).searchForBooks(query: query);
+        onSearchChanged(query, context);
       },
       decoration: InputDecoration(
         enabledBorder: buildOutlineInputBorder(),
