27d9e91f7c21563a96cebcf7c670451fad42d1b9
Mu Abdullah <mohamed553231@gmail.com>
2026-04-29 07:27:51 +0300

    Add SearchRepo

---
+++ lib/Features/search/domain/repos/search_repo.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class SearchRepo {
+  Future<Either<Failure,List<BookEntity>>> searchForBooks({required String query});
+}
